bffb2c8965b45af3a6eda6d773b3bc4566d0b5fe
Dennis Carlstrom <lazy@lazymammal.com>
2017-12-29 02:48:54 -0500

    tf.layers.dense for fully connected layers

--- mnist/bias_layer.py
+++ mnist/bias_layer.py
@@ -5,9 +5,6 @@ import tensorflow as tf
 
 
 def make_model(x, y_):
-    input_size = int(x.get_shape()[-1])
     output_size = int(y_.get_shape()[-1])
-    W = tf.Variable(tf.random_uniform(shape=[input_size, output_size], dtype=tf.float32))
-    b = tf.Variable(tf.random_uniform(shape=[output_size], dtype=tf.float32))
-    y = tf.matmul(x, W) + b
-    return y
+    layer = tf.layers.dense(inputs=x, units=output_size)
+    return layer
--- mnist/tanh_layer.py
+++ mnist/tanh_layer.py
@@ -7,12 +7,9 @@ import tensorflow as tf
 def make_model(x, y_):
     input_size = int(x.get_shape()[-1])
     output_size = int(y_.get_shape()[-1])
-    hidden_size = (input_size + output_size)//2
+    hidden_size = int((input_size + output_size)/2)
 
-    W1 = tf.Variable(tf.random_uniform(shape=[input_size, hidden_size], dtype=tf.float32))
-    L1 = tf.tanh( tf.matmul(x, W1) )
-
-    W2 = tf.Variable(tf.random_uniform(shape=[hidden_size, output_size], dtype=tf.float32))
-    L2 = tf.matmul(L1, W2)
+    L1 = tf.layers.dense(inputs=x, units=hidden_size, activation=tf.nn.tanh)
+    L2 = tf.layers.dense(inputs=L1, units=output_size)
 
     return L2
