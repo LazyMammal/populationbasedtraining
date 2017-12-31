005d58bb6003c9ed5b7911300c556c8dfab0b626
Dennis Carlstrom <lazy@lazymammal.com>
2017-12-30 23:44:33 -0500

    multiple subplots

--- mnist/plot.py
+++ mnist/plot.py
@@ -11,8 +11,12 @@ def main(args):
     table = np.loadtxt(args.file, delimiter=',', usecols=(0, 1, 5, 6, 7, 8))
     steporder = np.reshape(table, (10, -1, 6))
     workerorder = np.swapaxes(steporder, 0, 1)
-    for worker in workerorder:
-        plt.plot(worker[:, 0], worker[:, 5], alpha=0.5)
+
+    for col, title, plotnum in [(5, 'test accuracy', 221), (4, 'train accuracy', 222), (3, 'batch size', 223), (2, 'learning rate', 224)]:
+        plt.subplot(plotnum)
+        plt.title(title)
+        for worker in workerorder:
+            plt.plot(worker[:, 0], worker[:, col], alpha=0.5)
     plt.show()
 
 
