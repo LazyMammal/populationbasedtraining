0cf92a7fcd8e4e97a1ab60c1e421fd681036b388
Dennis Carlstrom <lazy@lazymammal.com>
2018-01-03 19:30:21 -0500

    add some plot types

--- mnist/plot.py
+++ mnist/plot.py
@@ -8,10 +8,44 @@ import matplotlib.pyplot as plt
 
 
 def main(args):
-    table = np.loadtxt(args.file, delimiter=',', usecols=(0, 1, 5, 6, 7, 8))
-    steporder = np.reshape(table, (10, -1, 6))
+    table = np.loadtxt(args.file, delimiter=',', usecols=(0, 1, 5, 6, 7, 8), skiprows=args.skiprows)
+    maxworker = int(max(np.array(table)[:, 1]) + 1)
+    steporder = np.reshape(table, (maxworker, -1, 6))
     workerorder = np.swapaxes(steporder, 0, 1)
 
+    # mainplots(workerorder)
+    # test_vs_train(workerorder)
+    # test_div_train(workerorder)
+    overfit(workerorder)
+
+def overfit(workerorder):
+    title = '% overfit'
+    plt.title(title)
+    plt.xlabel("steps")
+    plt.ylabel("(test - train) / test")
+    for worker in workerorder:
+        plt.plot(worker[:, 0], ((1-worker[:, 5]) - (1-worker[:, 4])) / (1-worker[:, 5]), alpha=0.5, marker='o')
+    plt.show()
+
+def test_div_train(workerorder):
+    title = 'test / train'
+    plt.title(title)
+    plt.xlabel("steps")
+    plt.ylabel("test / train")
+    for worker in workerorder:
+        plt.plot(worker[:, 0], (1-worker[:, 5]) / (1-worker[:, 4]), alpha=0.5, marker='o')
+    plt.show()
+
+def test_vs_train(workerorder):
+    title = 'test vs train'
+    plt.title(title)
+    plt.xlabel("train")
+    plt.ylabel("test")
+    for worker in workerorder:
+        plt.plot(1-worker[:, 4], 1-worker[:, 5], alpha=0.5, marker='o')
+    plt.show()
+
+def mainplots(workerorder):
     for col, title, plotnum in [(5, 'test accuracy', 221), (4, 'train accuracy', 222), (3, 'batch size', 223), (2, 'learning rate', 224)]:
         plt.subplot(plotnum)
         plt.title(title)
@@ -24,4 +58,6 @@ if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument('--file', nargs='?',
                         default="csv\workers.csv", help="input file")
+    parser.add_argument('--skiprows', nargs='?', type=int,
+                        default=0, help="Skip the first skiprows lines; default: 0")
     main(parser.parse_args())
