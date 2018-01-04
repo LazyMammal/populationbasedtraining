8ca6ef84bbeac97d2d81417200dac3f12280adc0
Dennis Carlstrom <lazy@lazymammal.com>
2018-01-04 17:28:09 -0500

    plot scaling options

--- mnist/plot.py
+++ mnist/plot.py
@@ -13,7 +13,10 @@ def main(args):
     maxworker = int(max(np.array(table)[:, 1]) + 1)
     workerorder = np.reshape(table[np.lexsort((table[:,0],table[:,1]))], (maxworker, -1, 6))
 
-    mainplots(workerorder)
+    if args.logplot:
+        mainplots(workerorder, [None, None, None, 2])
+    else:
+        mainplots(workerorder)
     overfit(workerorder)
 
 def overfit(workerorder):
@@ -32,9 +35,14 @@ def overfit(workerorder):
         plt.plot(worker[:, 0], (1-worker[:, 5]) / (1-worker[:, 4]), alpha=0.5, marker='o')
     plt.show()
 
-def mainplots(workerorder):
-    for col, title, plotnum in [(5, 'test accuracy', 221), (4, 'train accuracy', 222), (3, 'batch size', 223), (2, 'learning rate', 224)]:
+
+def mainplots(workerorder, logargs=[None, None, None, None], yscaleargs=[(0.0,1.0),(0.0,1.0),None,None]):
+    for col, title, plotnum, logplot, yscale in [(a, b, c, d, e) for (a, b, c), d, e in zip([(5, 'test accuracy', 221), (4, 'train accuracy', 222), (3, 'batch size', 223), (2, 'learning rate', 224)], logargs, yscaleargs)]:
         plt.subplot(plotnum)
+        if not logplot is None:
+            plt.semilogy(logplot)
+        if not yscale is None:
+            plt.ylim(*yscale)
         plt.title(title)
         for worker in workerorder:
             plt.plot(worker[:, 0], worker[:, col], alpha=0.5)
@@ -47,4 +55,6 @@ if __name__ == '__main__':
                         default="csv\workers.csv", help="input file")
     parser.add_argument('--skiprows', nargs='?', type=int,
                         default=0, help="Skip the first skiprows lines; default: 0")
+    parser.add_argument('--logplot', nargs='?', type=bool,
+                        default=False, help="Use log scale y-axis for hyperparameters")
     main(parser.parse_args())
