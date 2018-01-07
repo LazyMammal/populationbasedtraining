ce08f635932dc733b622eb366bb3ad06cffc4ea1
Dennis Carlstrom <lazy@lazymammal.com>
2018-01-06 21:11:18 -0500

    random restart

--- mnist/gridsearch.py
+++ mnist/gridsearch.py
@@ -9,6 +9,7 @@ from timer import Timer
 import mnist
 from test_accuracy import test_accuracy
 import hparams as hp
+import workers as workers_mod
 
 
 def main(args):
@@ -35,9 +36,9 @@ def multi_random(dataset, popsize, train_time, training_steps, test_size=1000):
             for step in range(1, training_steps + 1):
                 print('%d, ' % step, end='')
                 print('%d, ' % worker, end='')
-                train_graph(sess, dataset, train_time, test_size,
-                            batch_size, learn_rate)
-            print('# worker time %3.1fs, ' % step_time.split(), end='')
+                workers_mod.train_graph(sess, dataset, train_time, test_size,
+                                        batch_size, learn_rate)
+            print('# worker time %3.1fs' % worker_time.split())
 
 
 def search_grid(dataset, popsize, train_time, training_steps, test_size=1000):
@@ -50,42 +51,9 @@ def search_grid(dataset, popsize, train_time, training_steps, test_size=1000):
                 for step in range(1, training_steps + 1):
                     print('%d, ' % step, end='')
                     print('%d, ' % worker, end='')
-                    train_graph(sess, dataset, train_time, test_size,
-                                batch_size, learn_rate)
+                    workers_mod.train_graph(sess, dataset, train_time, test_size,
+                                            batch_size, learn_rate)
                 worker += 1
-
-
-def train_graph(sess, dataset, train_time, test_size, batch_size, learn_rate):
-    train_step = tf.get_collection('train_step')[0]
-    x = tf.get_collection('x')[0]
-    y_ = tf.get_collection('y_')[0]
-    learning_rate = tf.get_collection('learning_rate')[0]
-    accuracy = tf.get_collection('accuracy')[0]
-
-    batch_time = Timer()
-    iterations = 50
-    total_iterations = 0
-    count = 0
-    while batch_time.elapsed() < train_time:
-        mnist.iterate_training(sess, iterations, batch_size, learn_rate,
-                               dataset, x, y_, train_step, learning_rate)
-        count += 1
-        total_iterations += iterations
-
-    print('%d, %f, %d, ' %
-          (total_iterations * batch_size, batch_time.split(), count), end='')
-    print('%g, ' % learn_rate, end='')
-    print('%d, ' % batch_size, end='')
-
-    testdata_size = len(dataset.test.labels)
-    trainscore = test_accuracy(
-        sess, dataset.train, testdata_size, test_size, x, y_, accuracy, True)
-    testscore = test_accuracy(
-        sess, dataset.test, testdata_size, test_size, x, y_, accuracy)
-
-    print('%f, ' % trainscore, end='')
-    print('%f, ' % testscore, end='')
-    print('%f' % batch_time.split())
 
 
 if __name__ == '__main__':
